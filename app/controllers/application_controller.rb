c338c7648ae213621507062d95508743c3a05c2c
airton.vasconcelosjr <airton.vasconcelosjr@gmail.com>
2024-08-20 17:01:05 -0300

    Fix: fixing tailwind install problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protected
 
   def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
+    posts_path
   end
 end
